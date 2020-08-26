bd10dde97d6d15e6df98d9b3b646057ed4d46a10
Michael Allan <mike@reluk.ca>
2020-08-26 10:10:33 -0400

    Targeting.

---
+++ Target.java
@@ -0,0 +1,14 @@
+package building;
+
+
+/** A build target particular to this software project.
+  */
+public enum Target {
+
+
+    /** The software builder of this project, compiled from Java source code into `.class` files.
+      */
+    SELF; }
+
+
+                                       // Copyright © 2020, Michael Allan and contributors.  Licence MIT.
