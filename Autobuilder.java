c50152f20653dfbe49d8d2e31227d2b153addac7
Michael Allan <mike@reluk.ca>
2020-08-31 20:41:03 -0400

    Bootstrapping into an autobuilder.

---
+++ Autobuilder.java
@@ -0,0 +1,10 @@
+package building;
+
+
+/** A builder of software builders.  Compiled alone and invoked by the build command, it relies on
+  * no other code for its implentation.
+  */
+public final class Autobuilder {}
+
+
+                                       // Copyright © 2020, Michael Allan and contributors.  Licence MIT.
