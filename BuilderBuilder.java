0df04b9fa637bfe9bbafd17585d4fd9243f04e22
Michael Allan <mike@reluk.ca>
2020-09-18 18:43:27 -0400

    Clarify.

---
+++ BuilderBuilder.java
@@ -0,0 +1,10 @@
+package building;
+
+
+/** A builder of software builders.  Compiled alone and invoked by the build command,
+  * it works without relying on other code.
+  */
+public final class BuilderBuilder {}
+
+
+                                                        // Copyright © 2020  Michael Allan.  Licence MIT.
