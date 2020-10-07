7a941f0ff596d5f7fdf1a64df4ca8aecdb24a49b
Michael Allan <mike@reluk.ca>
2020-10-07 03:00:55 -0400

    Normalize.

--- BuilderBuilderD.java
+++ BuilderBuilderD.java
@@ -22,14 +22,14 @@ public class BuilderBuilderD implements BuilderBuilder {
    // ━━━  B u i l d e r   B u i l d e r  ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 
-    public final String projectPackage() { return projectPackage; }
+    public final @Override String projectPackage() { return projectPackage; }
 
 
         private final String projectPackage;
 
 
 
-    public final Path projectPath() { return projectPath; }
+    public final @Override Path projectPath() { return projectPath; }
 
 
         private final Path projectPath; }
